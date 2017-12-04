2d8ec623799d88417cecea26b3c10f7f45b7318e
2109 <cai32109@gmail.com>
2017-12-04 20:55:47 +0800

    update

--- test.lua
+++ test.lua
@@ -1,25 +1,39 @@
 local dump = require "dump"
 local util = require "util"
 
-local mrq = {
-	girl = "hx",
-	age = 29,
-	hx = {
-		[1] = "1",
-		["live"] = "th",
-		age = 24
-	},
-	haha = {
-		{1,2,3},
-		{"a" , b = {a = 2}},
-		a = "mrq",
-		{2,1},
-	}
-}
+ local Ab = {
+	 	person = {
+	 		{
+	 			name = "Alice",
+	 			id = 10000,
+	 			phone = {
+	 				{number = "123456789",type = 1},
+	 				{number = "87654321",type = 2},
+	 			}
+	 		},
+	 		{
+	 			name = "Bob",
+	 			id = 20000,
+	 			phone = {
+	 				{number = "01234567890",type = 3},
+	 			}
+	 		},
+	 	}
+	 }
 
-local str = dump.pack_sort(mrq)
+local str = dump.pack_sort(Ab)
 
-print(str)
-local t = dump.unpack(str)
+local now = os.time()
+for i = 1,1024*1024 do
+	local t = dump.unpack(str)
+end
 
-util.dump_table(t)
+print(os.time()-now)
+
+local now = os.time()
+str = "return"..str
+for i = 1,1024*1024 do
+	local t = load(str)()
+end
+
+print(os.time()-now)
